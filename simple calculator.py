1ba3ddb0ffcf238d14327ee5986f1fe62b9df9b5
Sadaf <s46319497@gmail.com>
2026-02-27 08:19:19 -0800

    this is the base file

---
+++ simple calculator.py
@@ -0,0 +1,23 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Entet choice (1/2/3/4):")
+
+if choice == "1":
+    print("Result:", num1 |+ num2)
+elif choice == "2":
+    print("Result:", num1 - num2)
+ elif choice == "3":
+    print("Result:", num1 * num2)
+elif choice == "4":
+    print("Result:",num1 / num2)
+else:
+    print("Invalid choice")               
+

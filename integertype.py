32c9864a396e005286db34d337c973ae94e367af
Sadaf <s46319497@gmail.com>
2026-02-27 09:15:43 -0800

    this is the base file

---
+++ integertype.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first integer:  "))
+b = int(input("Enter second integer: "))
+
+if a > b:
+    print("First number is greater")
+elif b > a:
+    print("Second number is greater")
+else:
+    print("Both numbers are equal")        
+ 
